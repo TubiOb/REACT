7477e1aef4dca9f2f19025374989214abcf4e3b1
TubiOb <Tubiobaloluwa@gmail.com>
2023-07-30 05:10:36 +0100

    completed the dashboard corrections

--- incover/src/components/ActivePlans.jsx
+++ incover/src/components/ActivePlans.jsx
@@ -9,7 +9,7 @@ const ActivePlans = () => {
   ];
   return (
     <div className='w-full px-6 py-4 h-auto items-center justify-between grid grid-cols-1'>
-        <div className='flex flex-wrap items-center justify-between p-2'>
+        <div className='flex flex-wrap items-center justify-between p-2 mb-1'>
           <h3 className='font-semibold'>Active Plans</h3>
           <div className='flex flex-row items-center justify-between gap-0.5 hover:cursor-pointer'>
             <h3 className='font-medium text-sm'>View More</h3>
