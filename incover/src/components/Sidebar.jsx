6d6e788938887e86b161ddf3ffdb8e4968774f72
TubiOb <Tubiobaloluwa@gmail.com>
2023-08-03 16:21:30 +0100

    correcting responsiveness of sidebar

--- incover/src/components/Sidebar.jsx
+++ incover/src/components/Sidebar.jsx
@@ -11,6 +11,8 @@ import { CiExport } from 'react-icons/ci'
 
 const Sidebar = () => {
   const [activeItem, setActiveItem] = useState('dashboard');
+  const [isSidebarExpanded, setSidebarExpanded] = useState(false);
+  // const [isClaimsVisible, setClaimsVisible] = useState(true);
 
   const handleItemClick = (item) => {
     setActiveItem(item);
@@ -26,29 +28,37 @@ const Sidebar = () => {
     setProfileOptionVisible(false);
   };
 
+  const handleSidebarToggle = () => {
+    setSidebarExpanded(!isSidebarExpanded);
+  };
+
+  // const handleClaimsToggle = () => {
+  //   setClaimsVisible(!isClaimsVisible);
+  // };
+
   return (
-    <div className="w-40 md:w-64 flex flex-col bg-gray-50 text-gray-600 px-4 py-3 items-center justify-between top-0 bottom-0 left-0 fixed">
-        <img src={logo} alt="BOI Ins Broker" className='w-[150px] mx-auto mt-4'/>
+    <div className={`w-${isSidebarExpanded ? 'md:w-64' : '20'} flex flex-col bg-gray-50 text-gray-600 px-4 py-3 items-center justify-between top-0 bottom-0 left-0 fixed`}>
+        <img src={logo} alt="BOI Ins Broker" className='w-[95%] md:w-[80%] mx-auto mt-4'/>
 
         <ul className='grid grid-cols-1 justify-between gap-16 h-auto mx-auto w-[80%] -mt-16 md:-mt-36'>
           <ul className='w-full gap-1.5'>
-            <li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer ${activeItem === 'dashboard' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('dashboard')}><Link to='/dashboard'  className='flex'><MdOutlineSpaceDashboard size={15} /> dashboard</Link></li>
-            <li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer hover:text-green-700 ${activeItem === 'setup-guide' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('setup-guide')}><Link to='/setup-guide'  className='flex'><BsSubtract size={15}/>setup guide</Link></li>
+            <Link to='/dashboard'  className='flex'><li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer ${activeItem === 'dashboard' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('dashboard')}><MdOutlineSpaceDashboard size={15} /><p className='md:flex hidden'>dashboard</p></li></Link>
+            <Link to='/setup-guide'  className='flex'><li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer hover:text-green-700 ${activeItem === 'setup-guide' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('setup-guide')}><BsSubtract size={15}/><p className='md:flex hidden'>setup guide</p></li></Link>
           </ul>
 
           <ul className='w-full gap-1.5'>
-            <h3 className='text-gray-400 uppercase text-xs'>insurance</h3>
-            <li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer hover:text-green-700 ${activeItem === 'my-policies' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('my-policies')}><Link to='/policies'  className='flex'><GoShieldCheck size={15}/>my policies</Link></li>
-            <li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer hover:text-green-700 ${activeItem === 'my-claims' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('my-claims')}><RiBillLine size={15}/>my claims <IoIosArrowForward size={15} className='ml-3 md:ml-12'/></li>
+            <h3 className='text-gray-400 uppercase text-xs md:flex hidden'>insurance</h3>
+            <Link to='/policies'  className='flex'><li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer hover:text-green-700 ${activeItem === 'my-policies' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('my-policies')}><GoShieldCheck size={15}/><p className='md:flex hidden'>my policies</p></li></Link>
+            <li className={`capitalize text-xs md:text-sm flex flex-row items-center justify-start p-1.5 w-full gap-1 hover:cursor-pointer hover:text-green-700 ${activeItem === 'my-claims' ? 'bg-green-100 text-green-700 font-semibold rounded-xl' : ''}`} onClick={() => handleItemClick('my-claims')}><RiBillLine size={15}/><p className='md:flex hidden'>my claims</p><IoIosArrowForward size={15} className='ml-3 md:ml-12 md:flex hidden'/></li>
           </ul>
         </ul>
         
 
-        <ul className='w-[95%] mx-auto'>
-          <li className='flex relative flex-row p-0.5 md:p-1.5 justify-between items-center hover:shadow-2xl hover:cursor-pointer hover:rounded-md' >
+        <ul className='md:w-[95%] w-[80%] mx-auto'>
+          <li className='Profile flex relative flex-row p-0.5 md:p-1.5 justify-between items-center hover:shadow-2xl hover:cursor-pointer hover:rounded-md' onClick={handleSidebarToggle}>
             <div className='flex flex-row p-0.5 md:p-1 justify-center gap-3 items-center' onClick={handleLiItemClick}>
               <img src={myImage} alt="" className='rounded-full w-5 md:w-7 object-cover'/>
-              <p className='capitalize text-xs md:text-sm font-semibold '>fiyin oluwakemi</p>
+              <p className='capitalize text-xs md:text-sm font-semibold md:flex hidden'>fiyin oluwakemi</p>
               <RiArrowDownSLine size={20} />
             </div>
             {isProfileOptionVisible && (
