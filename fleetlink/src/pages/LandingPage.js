5446b4b3b09369ef45535a15d91ba0598bf5e657
TubiOb <Tubiobaloluwa@gmail.com>
2023-09-05 17:01:57 +0100

    Adding finishing touches to the Staff landing page

--- fleetlink/src/pages/LandingPage.js
+++ fleetlink/src/pages/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import backgound from '../assets/asia-2181888.jpg'
 import { HashLoader } from 'react-spinners'
 
 const LandingPage = () => {
@@ -25,20 +26,29 @@ const LandingPage = () => {
 
 
   return (
-    <div className='h-screen w-full'>
-      <div className='h-full w-full bg-cover bg-no-repeat bg-[url("~assets/asia-2181888.jpg")] backdrop-blur-lg'>
-        {loading && <div className="absolute inset-0 bg-transparent bg-opacity-80 backdrop-blur-2xl" />}
-        <div className='flex flex-col w-[70%] md:w-[50%] mx-auto absolute left-1/2 -translate-x-1/2 top-1/2 transform -translate-y-1/2 gap-3'>
-          {loading && <div className="absolute inset-0 bg-transparent bg-opacity-80 backdrop-blur-sm" />}
-          {/* <div className='items-center justify-center text-center p-1'> */}
-            <h3 className='text-white font-semibold text-4xl items-center justify-center text-center'>Connecting Workplaces, One Ride at a Time</h3>
-          {/* </div> */}
-          <div className='w-full md:w-[70%] md:mx-auto flex flex-row gap-7 items-center justify-between p-2 mt-2'>
-            <button type="submit" className='text-neutral-500 px-2 py-2 rounded-2xl outline-none border-none w-full md:w-[30%] mx-auto bg-white font-medium transition duration-300 hover:bg-emerald-500 hover:text-white hover:shadow-md hover:font-semibold text-sm md:text-lg' onClick={handleLoginClick}>Login</button>
-            <button type="submit" className='text-white px-2 py-2 rounded-2xl outline-none border-none w-full md:w-[30%] mx-auto bg-neutral-500 font-medium transition duration-300 hover:bg-emerald-500 hover:text-white hover:shadow-md hover:font-semibold text-sm md:text-lg' onClick={handleSignupClick}>Signup</button>
+    <div className='h-screen w-full relative bg-neutral-400'>
+      <div className='w-full h-full relative'>
+        <img src={backgound} alt="FleetLink" className='relative object-cover w-full h-full' />
+        <div className='absolute top-0 left-0 w-full h-full bg-black opacity-70 blur-xl'></div>
+      </div>
+      <div className='flex flex-col w-[70%] md:w-[50%] mx-auto absolute left-1/2 -translate-x-1/2 top-1/2 md:top-1/3 transform md:translate-y-1/2 -translate-y-1/2 gap-3'>
+        <h3 className='text-white font-semibold text-lg md:text-4xl items-center justify-start'>Connecting Workplaces, One Ride at a Time</h3>
+      </div>
+      <div className='bg-white w-full h-[30%] md:h-[40%] md:rounded-tl-3xl rounded-tr-3xl fixed bottom-0 left-0 py-9 px-4 flex flex-col'>
+        <div className='items-center justify-center w-full flex p-2'>
+          <h3 className='text-sm md:text-lg flex font-semibold'>Enhance your Workdays with Effortless Mobility</h3>
+        </div>
+        <div className='w-full md:w-[70%] md:mx-auto flex flex-row gap-7 items-center justify-between p-2 mt-4 md:mt-8'>
+          <button type="submit" className='text-neutral-500 px-2 py-2 rounded-xl outline-none border-none w-full md:w-[30%] mx-auto bg-white font-semibold shadow-neutral-800 shadow-2xl transition duration-300 hover:bg-emerald-500 hover:text-white hover:shadow-md hover:font-semibold text-sm md:text-lg flex items-center justify-center' onClick={handleLoginClick} disabled={loading}>Signin</button>
+          <button type="submit" className='text-white px-2 py-2 rounded-xl outline-none border-none w-full md:w-[30%] mx-auto bg-neutral-900 font-semibold shadow-neutral-800 shadow-2xl transition duration-300 hover:bg-emerald-500 hover:text-white hover:shadow-md hover:font-semibold text-sm md:text-lg md:flex hidden items-center justify-center' onClick={handleSignupClick} disabled={loading}>Signup</button>
+        </div>
+        <div className='flex flex-row-reverse md:hidden'>
+          <div>
+            <button type="submit">Or Create Account</button>
           </div>
         </div>
       </div>
+      {loading && <div className="absolute inset-0 bg-transparent w-full h-full bg-opacity-80 backdrop-blur-sm" />}
       {loading && (
         <div className="fixed left-1/2 -translate-x-1/2 top-1/2 transform -translate-y-1/2 items-center justify-center">
           <HashLoader color="#36d7b7" />
