909f9ccc7d668af7d16fb13808013ec2a97009f7
TubiOb <Tubiobaloluwa@gmail.com>
2023-09-21 11:52:29 +0100

    Correcting Dashboard

--- incover/src/components/QuickAction.jsx
+++ incover/src/components/QuickAction.jsx
@@ -9,26 +9,26 @@ const QuickAction = () => {
       <h3 className='font-semibold'>
         Quick Actions
       </h3>
-      <div className='grid md:grid-cols-3 grid-cols-1 items-center md:items-start justify-evenly w-full md:w-[60%] gap-6 md:gap-4 mt-2 p-4'>
+      <div className='md:flex-row flex-col items-center md:items-start justify-evenly w-full md:w-[1144px] md:gap-4 mt-2 p-4        md:justify-start gap-8 inline-flex '>
         
         <Link to='/policies/motorInsurance'>
-          <div className='grid grid-cols-1 items-center text-center justify-center gap-1 p-4 shadow-xl w-full  h-32 rounded-lg hover:cursor-pointer hover:shadow-2xl'>
-            <img src={Protection} alt="" className='mx-auto'/>
-            <p className='font-medium text-xs'>Motor Insurance</p>
+          <div className='flex-col items-center text-center justify-center px-4 py-6 bg-white shadow-xl w-[162px] h-[167px] rounded-2xl hover:cursor-pointer hover:shadow-2xl gap-4 inline-flex'>
+            <img src={Protection} alt="" className='mx-auto w-[60px] h-[60px]'/>
+            <p className="font-medium text-xs text-center text-neutral-600 md:text-base font-['Red Hat Display'] leading-normal">Motor Insurance</p>
           </div>
         </Link>
 
         <Link to='/policies'>
-          <div className='grid grid-cols-1 items-center text-center justify-center gap-1 p-4 shadow-xl w-full  h-32 rounded-lg hover:cursor-pointer hover:shadow-2xl'>
-            <img src={Selection} alt="" className='mx-auto'/>
-            <p className='font-medium text-xs'>View all products</p>
+          <div className='flex-col items-center text-center justify-center px-4 py-6 bg-white shadow-xl w-[162px] h-[167px] rounded-2xl hover:cursor-pointer hover:shadow-2xl gap-4 inline-flex'>
+            <img src={Selection} alt="" className='mx-auto w-[60px] h-[60px]'/>
+            <p className="font-medium text-xs text-center text-neutral-600 md:text-base font-['Red Hat Display'] leading-normal">View all products</p>
           </div>
         </Link>
         
         <Link to=''>
-          <div className='grid grid-cols-1 items-center text-center justify-center gap-1 p-4 shadow-xl w-full  h-32 rounded-lg hover:cursor-pointer hover:shadow-2xl'>
-            <img src={Claims} alt="" className='mx-auto'/>
-            <p className='font-medium text-xs'>File a claim</p>
+          <div className='flex-col items-center text-center justify-center px-4 py-6 bg-white shadow-xl w-[162px] h-[167px] rounded-2xl hover:cursor-pointer hover:shadow-2xl gap-4 inline-flex'>
+            <img src={Claims} alt="" className='mx-auto w-[60px] h-[60px]'/>
+            <p className="font-medium text-xs text-center text-neutral-600 md:text-base font-['Red Hat Display'] leading-normal">File a claim</p>
           </div>
         </Link>
         
