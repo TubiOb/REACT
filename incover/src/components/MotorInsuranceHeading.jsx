afafdb1a4f007626eb87ffc06ea7211192a0bc00
TubiOb <Tubiobaloluwa@gmail.com>
2023-09-25 12:23:15 +0100

    Corrected Icons

--- incover/src/components/MotorInsuranceHeading.jsx
+++ incover/src/components/MotorInsuranceHeading.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { MdOutlineAddBox } from 'react-icons/md'
+// import { MdOutlineAddBox } from 'react-icons/md'
 // import { useLocation } from 'react-router-dom'
+import Add from '../assets/add-square.svg'
 
 const MotorInsuranceHeading = () => {
     // const location = useLocation();
@@ -9,9 +10,11 @@ const MotorInsuranceHeading = () => {
   return (
     <div className='w-full top-0 sticky right-0 border-b border-b-neutral-200 h-16 m-0 items-center bg-white justify-between py-4 px-6 flex z-40'>
       <h2 className='font-semibold capitalize text-xs md:text-lg'>Motor Insurance</h2>
-      <div className='flex flex-row items-center text-xs justify-between gap-1.5 bg-green-700 rounded-lg p-1.5 text-white font-semibold hover:cursor-pointer'>
-        <MdOutlineAddBox size={18}/>
-        <h2>Add new policy</h2>
+      <div className="Buttons w-[135px] h-11 p-3 bg-green-800 rounded-lg justify-center items-center gap-2 inline-flex">
+          <div className="AddSquare w-5 h-5 relative">
+              <img src={Add} className="AddSquare w-5 h-5 left-0 top-0 absolute"/>
+          </div>
+          <div className="Button2 text-white text-xs font-medium font-['Red Hat Display'] leading-none">Add new policy</div>
       </div>
     </div>
   )
