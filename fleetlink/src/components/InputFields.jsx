c600a16bf095b4e22292d5e28661cc56cc1eaa77
TubiOb <Tubiobaloluwa@gmail.com>
2023-09-26 16:45:14 +0100

    Sorted the refreshing issue with sign in and sign up page

---
+++ fleetlink/src/components/InputFields.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
+
+const InputFields = ({ placeholder, setLoading }) => {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        if (inputRef.current) {
+            const input = inputRef.current;
+            const legend = input.previousSibling;
+            if (legend && placeholder) {
+                legend.textContent = placeholder;
+            }
+        }
+    }, [setLoading, placeholder]);
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+  return (
+    <div className='border-none outline-none mb-2 w-[90%] mx-auto gap-2 inline-flex flex-col'>
+        <fieldset className='border-none outline-none'>
+            <legend className='text-neutral-600 font-semibold text-xs ml-2 border-none outline-none'>{placeholder}</legend>
+            <div className='w-full h-9 px-3 flex flex-row items-center justify-between gap-2.5 bg-neutral-200 rounded-md'>
+                <input 
+                ref={inputRef}
+                type={placeholder === 'Password' ? (passwordVisible ? 'text' : 'password') : 'text'}
+                className='w-full border-none outline-none p-1 text-sm bg-neutral-200'
+                placeholder={placeholder}/>
+                {placeholder === 'Password' && (
+                    <div className='w-5 h-5 relative secure'>
+                        {passwordVisible ? (
+                        <AiOutlineEyeInvisible className="cursor-pointer left-0 w-5 h-5 mr-1"  onClick={togglePasswordVisibility} />
+                        ) : (
+                        <AiOutlineEye className="cursor-pointer left-0 w-5 h-5 mr-1"  onClick={togglePasswordVisibility} />
+                        )}
+                    </div>
+                )}
+            </div>
+        </fieldset>
+    </div>
+  )
+}
+
+export default InputFields
---
+++ fleetlink/src/components/LoginForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import InputFields from '../components/InputFields'
+
+const LoginForm = () => {
+
+    
+  return (
+    <div className='w-full md:w-[50%] h-screen bg-inherit flex flex-col items-center justify-center p-4 mb-3 relative'>
+        <div className='mx-auto mt-8 md:mt-0 bg-white outline-none border-none rounded-xl shadow-sm shadow-neutral-50 w-[85%] md:w-[65%] inline-flex flex-col h-64 gap-4 px-2 py-5'>
+            <InputFields placeholder='Email Address'/>
+            <InputFields placeholder='Password'/>
+
+            <div className='w-[90%] flex flex-col mx-auto'>
+                <button type="submit" className='text-white px-2 py-2 rounded-xl w-full mx-auto bg-neutral-900 font-semibold shadow-neutral-800 shadow-2xl transition duration-300 hover:bg-white hover:text-neutral-900 hover:shadow-md hover:font-semibold hover:border hover:border-neutral-300 hover:shadow-neutral-300 text-sm md:text-lg flex items-center justify-center' >Sign In</button>
+                <div className='text-neutral-600 font-semibold text-xs ml-auto mt-1.5 mr-2 flex flex-row-reverse hover:cursor-pointer'>Forgot Password?</div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default LoginForm
