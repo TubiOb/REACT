4c35adc43af885645e03b51ea1c1bd04610e3b4f
TubiOb <Tubiobaloluwa@gmail.com>
2023-10-12 19:21:11 +0100

    Added transition and functionality to the Header Lis

--- fleetlink/src/components/Header.jsx
+++ fleetlink/src/components/Header.jsx
@@ -1,27 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import UserProfile from '../assets/IMG_20210903_102641.jpg'
 
 const Header = () => {
+  const location = useLocation();
+  // const isHome = location.pathname === '/home';
+
+  const menuItems = [
+      { name: 'Home', link: '/home' },
+      { name: 'Pick-up Schedule', link: '/pickupSchedule' },
+      { name: 'Updates', link: '/updates' },
+  ];
+
   return (
     <div className='w-full h-20 flex flex-row items-center justify-between fixed top-0 left-0 gap-2 py-1 px-6 shadow-bottom'>
         <div className='flex p-1 text-base md:text-lg font-semibold'>
-          <Link ><h2>FleetLink</h2></Link>
+          <Link to='/home'><h2>FleetLink</h2></Link>
         </div>
 
           <div className='md:flex p-1 items-center hidden'>
             <ul className='flex flex-row items-center justify-between gap-24 p-1'>
-              <Link to='/home'><li>Home</li></Link>
-              <Link to='/pickupSchedule'><li>Pick-up Schedule</li></Link>
-              <Link to='/updates'><li>Updates</li></Link>
+                {menuItems.map((menuItem) => (
+                    <Link to={menuItem.link} key={menuItem.link}>
+                        <li className={location.pathname === menuItem.link ? 'active-link font-semibold transition duration-500' : ''}>
+                            {menuItem.name}
+                        </li>
+                    </Link>
+                ))}
             </ul>
           </div>
 
           <div className='rounded-full w-7 h-7 md:w-9 md:h-9 overflow-hidden'>
             <a href="/profile" className='flex w-full h-full'>
-            
               <img src={UserProfile} alt=""  className='object-cover w-full h-full'/>
-            
             </a>
           </div>
         
