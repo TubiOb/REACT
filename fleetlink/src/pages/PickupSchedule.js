574d06f3fcbe8d8ec75a23810768fef6a6b39031
TubiOb <Tubiobaloluwa@gmail.com>
2023-10-17 23:07:01 +0100

    Working on the Schedule Page

--- fleetlink/src/pages/PickupSchedule.js
+++ fleetlink/src/pages/PickupSchedule.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/Header'
 import MobileNavigation from '../components/MobileNavigation'
 import Footer from '../components/Footer'
+import SchedulingForm from '../components/SchedulingForm'
 
 const PickupSchedule = () => {
   return (
-    <div className='w-full h-screen bg-white flex justify-center'>
+    <div className='w-full h-full bg-white flex flex-col justify-center'>
       <Header />
+      <SchedulingForm />
       <MobileNavigation />
       <Footer />
         
