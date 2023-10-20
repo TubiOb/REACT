4ec75bfe1fd5b3ad86d8fcf861a8cc8229a3279b
TubiOb <Tubiobaloluwa@gmail.com>
2023-10-20 16:14:50 +0100

    Finally getting headway with the dates

--- fleetlink/src/components/BookingCalendar.jsx
+++ fleetlink/src/components/BookingCalendar.jsx
@@ -8,7 +8,7 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import '../CustomCalendar.css';
-import { /* isAfter, addDays, */ isBefore,  differenceInDays } from 'date-fns';
+import { /* isAfter, addDays, */ isBefore } from 'date-fns';
 
 function BookingCalendar({ onDateSelect }) {
     const [dateRange, setDateRange] = useState([null, null]);
@@ -16,38 +16,6 @@ function BookingCalendar({ onDateSelect }) {
     const handleDateClick = (date) => {
         setDateRange(prevDateRange => {
             const [startDate, endDate] = prevDateRange;
-            
-            // if (!startDate || (startDate && endDate)) {
-            //     // Set the start date if it's empty or both start and end are set
-            //     return [date, endDate];
-            // } else {
-            //     // Check if the selected date range is within the allowed range (e.g., 7 days)
-            //     const isWithinAllowedRange = isBefore(date, addDays(startDate, 7));
-            //     // Set the end date
-            //     if (isAfter(date, startDate)) {
-            //         return [startDate, date];
-            //     } else {
-            //         return [date, startDate];
-            //     }
-            // }
-
-            // if (!startDate) {
-            //     // Set the start date if it's empty
-            //     return [date, null];
-            // } else if (!endDate) {
-            //     // If start date is set but end date is not, set the end date if within the allowed range
-            //     const isWithinAllowedRange = isBefore(date, addDays(startDate, 7));
-            //     if (isWithinAllowedRange) {
-            //         return [startDate, date];
-            //     } else {
-            //         // Handle this case as needed, e.g., show an error message
-            //         console.log("Date selection is outside the allowed range.");
-            //         return [startDate, null];
-            //     }
-            // } else {
-            //     // Both dates are set, start a new selection
-            //     return [date, null];
-            // }
             
 
             const today = new Date();
@@ -86,17 +54,25 @@ function BookingCalendar({ onDateSelect }) {
             <div className="mb-4 border-none">
                 <div className="calendar bg-white border-none">
                     <Calendar
-                        value={dateRange}
-                        onChange={(date) => setDateRange(date)}
+                        value={dateRange[0]}
+                        onChange={(date) => {
+                            if (!dateRange[0] || dateRange[1]) {
+                                // If there's no start date or both start and end dates are set, start a new selection
+                                setDateRange([date, null]);
+                            } else {
+                                // If there's a start date but no end date, set the end date
+                                setDateRange([dateRange[0], date]);
+                            }
+                        }}
                         selectRange
                         onClickDay={handleDateClick}
                         // className="bg-neutral-500 text-black border-none"
-                        tileClassName={({ date, view }) => {
-                            if (dateRange[0] && dateRange[1]) {
-                            if (
-                                date >= dateRange[0] &&
-                                date <= dateRange[1]
-                            ) {
+                        tileClassName={({ date }) => {
+                            if (date >= dateRange[0] && date <= dateRange[1]) {
+                                if (
+                                    date >= dateRange[0] &&
+                                    date <= dateRange[1]
+                                ) {
                                 return "text-white rounded-full";
                             }
                             }
