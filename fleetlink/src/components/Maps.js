2128d895bec6e98333e17bcee6608dca7083a443
TubiOb <Tubiobaloluwa@gmail.com>
2023-10-31 17:53:19 +0100

    I think I finally got the map to work

--- fleetlink/src/components/Maps.js
+++ fleetlink/src/components/Maps.js
@@ -1,3 +1,4 @@
+// import { MarkerClusterGroup } from 'leaflet'
 import React from 'react'
 import { MapContainer, Marker, Popup, TileLayer, /* useMap */ } from 'react-leaflet'
 // import 'leaflet/dist/leaflet.css'
@@ -6,12 +7,13 @@ const Maps = ({ position, zoom, markers }) => {
   return (
     <MapContainer id="map" center={position} zoom={zoom}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-          {markers.map((marker, index) => (
-            <Marker key={index} position={marker.position}>
-              <Popup>{marker.name}</Popup>
-            </Marker>
-          ))}
+        {/* <MarkerClusterGroup> */}
+            {markers.map((marker, index) => (
+              <Marker key={index} position={marker.position}>
+                <Popup>{marker.name}</Popup>
+              </Marker>
+            ))}
+        {/* </MarkerClusterGroup> */}
 
     </MapContainer>
   )
