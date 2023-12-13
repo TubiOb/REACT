e97436b097cfa02c0c863335c35977750ab29b5d
TubiOb <Tubiobaloluwa@gmail.com>
2023-12-13 20:46:37 +0100

    Update

--- fleetlink/src/components/SignupForm.jsx
+++ fleetlink/src/components/SignupForm.jsx
@@ -117,12 +117,12 @@ const SignupForm = () => {
   return (
     <div className='w-full md:w-[50%] h-screen bg-neutral-800 flex flex-col items-center justify-center p-4 relative'>
         <form onSubmit={handleSave} className='mx-auto bg-white outline-none border-none rounded-xl shadow-sm shadow-neutral-50 w-[85%] md:w-[65%] inline-flex flex-col h-auto gap-4 px-2 py-5'>
-            <InputFields placeholder='Full Name' onInputChange={(value) => handleInputChange(value, 'fullName')}/>
-            <InputFields placeholder='Email Address' onInputChange={(value) => handleInputChange(value, 'emailAddress')}/>
-            <InputFields placeholder='Phone Number' onInputChange={(value) => handleInputChange(value, 'phoneNumber')}/>
-            <InputFields placeholder='Address' onInputChange={(value) => handleInputChange(value, 'address')}/>
-            <InputFields placeholder='Password' onInputChange={(value) => handleInputChange(value, 'password')}/>
-            <InputFields placeholder='Confirm Password' onInputChange={(value) => handleInputChange(value, 'confirmPassword')}/>
+            <InputFields placeholder='Full Name' onChange={(value) => handleInputChange(value, 'fullName')}/>
+            <InputFields placeholder='Email Address' onChange={(value) => handleInputChange(value, 'emailAddress')}/>
+            <InputFields placeholder='Phone Number' onChange={(value) => handleInputChange(value, 'phoneNumber')}/>
+            <InputFields placeholder='Address' onChange={(value) => handleInputChange(value, 'address')}/>
+            <InputFields placeholder='Password' onChange={(value) => handleInputChange(value, 'password')}/>
+            <InputFields placeholder='Confirm Password' onChange={(value) => handleInputChange(value, 'confirmPassword')}/>
 
             <div className='w-[90%] flex flex-col mx-auto'>
                 <button type="submit" className='text-white px-2 py-2 rounded-xl w-full mx-auto bg-neutral-900 font-semibold shadow-neutral-800 shadow-2xl transition duration-300 hover:bg-white hover:text-neutral-900 hover:shadow-md hover:font-semibold hover:border hover:border-neutral-300 hover:shadow-neutral-300 text-sm md:text-lg flex items-center justify-center' >Sign Up</button>
